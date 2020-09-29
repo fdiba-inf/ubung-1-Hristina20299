80ebff8b7c5c3107ffead7728058a1e0b6cebad3
Hristina20299 <hristina.velkova.27@gmail.com>
2020-09-29 18:56:48 +0000

    -

--- src/exercise1/HelloFDIBA.java
+++ src/exercise1/HelloFDIBA.java
@@ -3,7 +3,7 @@ package exercise1;
 public class HelloFDIBA {
 
   public static void main(String[] args) {
-    System.out.println("hello Fdiba"); 
+    System.out.println("Hello Fdiba"); 
     }
     
 }
